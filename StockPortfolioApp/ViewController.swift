c8880896cfd7d039724ae950de04d2ab5edaee80
Sujay Bhowmick <sujay.bhowmick@sentifi.com>
2018-06-04 22:01:02 +0700

    Initial Commit

---
+++ StockPortfolioApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StockPortfolioApp
+//
+//  Created by Sujay Bhowmick on 6/4/18.
+//  Copyright © 2018 Sujay Bhowmick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
